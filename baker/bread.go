34d510b3ae9e2cffebb86dbb920ee44608800e27
Sean Boyle <sean.boyle@pearson.com>
2020-06-03 09:51:05 -0600

    BakeBread method

--- baker/bread.go
+++ baker/bread.go
@@ -7,3 +7,14 @@ import (
 func MakeBread(i string, j string) {
 	fmt.Printf("%s + bread + %s\n", i, j)
 }
+
+func BakeBread(ingredients ...string) {
+
+	for i, ingredient := range ingredients {
+		fmt.Printf("Step %d. Adding %s\n", i, ingredient)
+	}
+
+	fmt.Println("Mixing all together...")
+	fmt.Println("Baking...")
+	fmt.Println("Done!")
+}
